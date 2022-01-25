5fa99f13d5c9c3e3414a13e766cdbdf15acae4fb
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-01-25 16:20:46 +0000

    G10 - Programação Orientada a Objetos - Classes e MVC

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser) {
+
+    console.log(dataUser);
+
     var tr = document.createElement("tr");
 
     tr.innerHTML = `
-    <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
@@ -15,7 +17,6 @@ function addLine(dataUser) {
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
-    </tr>
     `;
 
     document.getElementById("table-users").appendChild(tr);
@@ -46,6 +47,17 @@ document.getElementById("form-user-create").addEventListener("submit", function 
         }
     });
 
-    addLine(user);
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
+
+    addLine(objectUser);
 });
 
