e5734706e67595b1ac5790574aac4721d3cda253
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-01-25 00:23:39 +0000

    G9 - Trabalhando com Funções

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(dataUser) {
+    var tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+}
+
 // document.querySelectorAll("button").forEach(function(){
 
 //     this.addEventListener("click", function(){
@@ -12,20 +32,20 @@ var user = {};
 //     alert("oi botao submit");
 // })
 
-document.getElementById("form-user-create").addEventListener("submit", function(e){
+document.getElementById("form-user-create").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    fields.forEach(function(field, index){
+    fields.forEach(function (field, index) {
 
-        if (field.name == "gender"){
-            if (field.checked){
+        if (field.name == "gender") {
+            if (field.checked) {
                 user[field.name] = field.value;
             }
         } else {
             user[field.name] = field.value;
-        }    
+        }
     });
 
-    console.log(user);
+    addLine(user);
 });
 
